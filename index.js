71de3fae30968e24ec5ea163dae4d0cac296b1c7
Brandon Pretelt <brandonpretelt93@gmail.com>
2018-02-05 07:33:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -51,12 +51,12 @@ function total() {
 }
 
 function removeFromCart(item) {
-  /* for (var i = 0; i < cart.length; i++) {
+  for (var i = 0; i < cart.length; i++) {
      if (cart[i].hasOwnProperty(item)) {
        cart.splice(i, 1);
      }
      return cart;
-   }*/
+   }
 }
 
 function placeOrder(cardNumber) {
